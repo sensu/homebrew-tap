812615d9f5d3e2793e1ce39acba63cd0a7089f81
Sean Porter <portertech@gmail.com>
2016-02-25 16:06:07 -0800

    [sensu] omnibus pkg install scripts

--- sensu.rb
+++ sensu.rb
@@ -1,9 +1,9 @@
 class Sensu < Formula
   desc "A monitoring framework that aims to be simple, malleable, and scalable."
   homepage "https://sensuapp.org"
-  url "https://github.com/sensu/sensu-homebrew/archive/0.0.5.tar.gz"
+  url "https://github.com/sensu/sensu-homebrew/archive/0.0.6.tar.gz"
   version "0.22.0"
-  sha256 "a6c1e7d2a815a19b38dcd1bd3071956ec06290f8da52aa2591897bff0281af3b"
+  sha256 "cc456a97466860be8369d105e3a249538234b183afbf9d2c54ccb446c6de2c7b"
 
   depends_on "openssl" => "universal"
   depends_on "readline"
@@ -15,5 +15,6 @@ class Sensu < Formula
     system "gem", "install", "sensu", "--version=#{version}", "--no-rdoc", "--no-ri"
     system "cp", "-r", "bin", "#{prefix}/"
     system "cp", "-r", "Library", "#{prefix}/"
+    system "cp", "-r", "scripts", "#{prefix}/"
   end
 end
