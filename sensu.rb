3f133ac99013db82cd52c1541e1eaf4ba16cc077
Sean Porter <portertech@gmail.com>
2016-02-25 17:55:59 -0800

    [sensu] postinstall fixes

--- sensu.rb
+++ sensu.rb
@@ -1,9 +1,9 @@
 class Sensu < Formula
   desc "A monitoring framework that aims to be simple, malleable, and scalable."
   homepage "https://sensuapp.org"
-  url "https://github.com/sensu/sensu-homebrew/archive/0.0.8.tar.gz"
+  url "https://github.com/sensu/sensu-homebrew/archive/0.0.9.tar.gz"
   version "0.22.0"
-  sha256 "53e521039910a466440e17e1328a9521ff6a2bd11bed55dc1f2982059712fa02"
+  sha256 "6a379484d91ba437b7ec0aaecb4fc33ce90d5363b4717c403285f0e9418b1fdd"
 
   depends_on "openssl" => "universal"
   depends_on "readline"
