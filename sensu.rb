b7fbedce165e55106aa92b8b1670b6bc6b28277e
Sean Porter <portertech@gmail.com>
2016-02-25 20:28:48 -0800

    [sensu] sensu-install stub needed +x

--- sensu.rb
+++ sensu.rb
@@ -1,9 +1,9 @@
 class Sensu < Formula
   desc "A monitoring framework that aims to be simple, malleable, and scalable."
   homepage "https://sensuapp.org"
-  url "https://github.com/sensu/sensu-homebrew/archive/0.0.11.tar.gz"
+  url "https://github.com/sensu/sensu-homebrew/archive/0.0.12.tar.gz"
   version "0.22.0"
-  sha256 "c5c29cd110d47300f7e3f15350385faf79e518ddd296deb9be1a99dd5efec4de"
+  sha256 "53d8c1afc0dea802267d8568ec23abc9a4c271267e2e3034b8b83ffcd331caed"
 
   depends_on "openssl" => "universal"
   depends_on "readline"
