e19a1341bfe85ca329ab892a19a62cbbdbed4ecc
Sean Porter <portertech@gmail.com>
2016-02-25 17:09:11 -0800

    [sensu] omnibus and installer script fixes

--- sensu.rb
+++ sensu.rb
@@ -1,9 +1,9 @@
 class Sensu < Formula
   desc "A monitoring framework that aims to be simple, malleable, and scalable."
   homepage "https://sensuapp.org"
-  url "https://github.com/sensu/sensu-homebrew/archive/0.0.7.tar.gz"
+  url "https://github.com/sensu/sensu-homebrew/archive/0.0.8.tar.gz"
   version "0.22.0"
-  sha256 "48ef8790eefdfb2f7fa665e9a7ed4be5fd651feb0698b353167fe17f9ae252dc"
+  sha256 "53e521039910a466440e17e1328a9521ff6a2bd11bed55dc1f2982059712fa02"
 
   depends_on "openssl" => "universal"
   depends_on "readline"
